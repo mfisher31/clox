d50331e8129e8d040f8eb1cd82b79d5917dbc2a7
Michael Fisher <mfisher31>
2024-05-19 20:55:02 -0400

    ch 18 - convert value to a variant type

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -12,6 +13,19 @@ static void resetStack() {
     vm.stackTop = vm.stack;
 }
 
+static void runtimeError (const char* format, ...) {
+    va_list args;
+    va_start (args, format);
+    vfprintf (stderr, format, args);
+    va_end (args);
+    fputs ("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line           = vm.chunk->lines[instruction];
+    fprintf (stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
 void initVM() {
     resetStack();
 }
@@ -19,14 +33,22 @@ void initVM() {
 void freeVM() {
 }
 
+static Value peek (int distance) {
+    return vm.stackTop[-1 - distance];
+}
+
 static InterpretResult run() {
 #define READ_BYTE()     (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)     \
-    do {                  \
-        double b = pop(); \
-        double a = pop(); \
-        push (a op b);    \
+#define BINARY_OP(valueType, op)                                \
+    do {                                                        \
+        if (! IS_NUMBER (peek (0)) || ! IS_NUMBER (peek (1))) { \
+            runtimeError ("Operands must be numbers.");         \
+            return INTERPRET_RUNTIME_ERROR;                     \
+        }                                                       \
+        double b = AS_NUMBER (pop());                           \
+        double a = AS_NUMBER (pop());                           \
+        push (valueType (a op b));                              \
     } while (false)
 
     for (;;) {
@@ -48,21 +70,28 @@ static InterpretResult run() {
                 push (constant);
                 break;
             }
+
             case OP_ADD:
-                BINARY_OP (+);
+                BINARY_OP (NUMBER_VAL, +);
                 break;
             case OP_SUBTRACT:
-                BINARY_OP (-);
+                BINARY_OP (NUMBER_VAL, -);
                 break;
             case OP_MULTIPLY:
-                BINARY_OP (*);
+                BINARY_OP (NUMBER_VAL, *);
                 break;
             case OP_DIVIDE:
-                BINARY_OP (/);
+                BINARY_OP (NUMBER_VAL, /);
                 break;
-            case OP_NEGATE:
-                push (-pop());
+
+            case OP_NEGATE: {
+                if (! IS_NUMBER (peek (0))) {
+                    runtimeError ("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push (NUMBER_VAL (-AS_NUMBER (pop())));
                 break;
+            }
             case OP_RETURN:
                 printValue (pop());
                 printf ("\n");
